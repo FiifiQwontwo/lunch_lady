29b9a25c62a81fdabe0276d13f22dc29b0f61671
FiifiQwontwo <quophi555@gmail.com>
2024-02-18 17:53:47 +0000

    hi

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from course.models import Course
 from school.models import School
 from type.models import Type
 from faculty.models import Faculty
+from faculty.slugify import fac_slugify
 
 
 # Create your models here.
@@ -38,6 +39,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
     is_student = models.BooleanField(default=False)
     is_lecturer = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=100, unique=True)
 
     objects = CustomUserManager()
 
@@ -46,6 +48,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def save(self, *args, **kwargs):
+        self.slug = fac_slugify(self.last_name)
+        super(CustomUser, self).save(*args, **kwargs)
 
 
 
@@ -57,6 +63,7 @@ class Student(models.Model):
     level = models.CharField(max_length=15, choices=LevelChoices, help_text='your student level', default=1, blank=True,null=True)
     phone = models.CharField(max_length=15,blank=True,null=True)
     course = models.ForeignKey(Course, on_delete=models.RESTRICT, related_name='students_course', blank=True, null=True)
+    slug = models.SlugField(max_length=100, unique=True)
 
 
     def __str__(self):
@@ -64,6 +71,11 @@ class Student(models.Model):
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        self.slug = fac_slugify(self.index_number)
+        super(Student, self).save(*args, **kwargs)
+
 
 
 class Lecturer(models.Model):
@@ -71,6 +83,7 @@ class Lecturer(models.Model):
     staff_id = models.CharField(max_length=100, unique=True)
     phone = models.CharField(max_length=11, unique=True)
     faculty = models.ForeignKey(Faculty, on_delete=models.RESTRICT, related_name="Staff_faculty")
+    slug = models.SlugField(max_length=100, unique=True)
 
 
     def __str__(self):
@@ -79,9 +92,14 @@ class Lecturer(models.Model):
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+    def save(self, *args, **kwargs):
+        self.slug = fac_slugify(self.staff_id)
+        super(Lecturer, self).save(*args, **kwargs)
+
 class UserAgentInfo(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     user_agent = models.CharField(max_length=255)
+    user_ip = models.GenericIPAddressField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
